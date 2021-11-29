501b291dbbc712ad082c6b4bd54191f2a390ad10
Yuval Lande <landeyuval@campus.technion.ac.il>
2021-11-29 13:14:37 +0200

    changed product and order behavior. added generic function to interact with these structs as a part of a set

--- Wet/matamikya.c
+++ Wet/matamikya.c
@@ -3,64 +3,85 @@
 #include "set.h"
 #include <stdlib.h>
 #include <string.h>
+#define RETURN_IF_NULL(pointer, return_value)\
+        if(pointer == NULL){\
+            return return_value;\
+        }\
 
 typedef struct product_t
 {
     char* name;
     unsigned int id;
     MtmProductData product_data;
+    MtmCopyData product_data_copy_function;
+    MtmFreeData product_data_free_function;
+    MtmGetProductPrice product_data_get_price;
     MatamikyaAmountType amount_type;
 }*Product;
 
-static Product createProduct(const char* name, const unsigned int id, 
+static ASElement createProduct(const char* name, const unsigned int id, 
                             MtmProductData product_data, MatamikyaAmountType amount_type,
-                            MtmCopyData copy_function)
+                            MtmCopyData copy_function, MtmFreeData free_function,
+                            MtmGetProductPrice price_function)
 {
     Product product = malloc(sizeof(*product));
-    if(product == NULL){
+    RETURN_IF_NULL(product, NULL);
+    product->name = malloc(sizeof(*name));
+    if(product->name == NULL){
+        free(product);
         return NULL;
     }
     strcpy(product->name, name);
     product->id = id;
     product->product_data = copy_function(product_data);
+    if(product->product_data == NULL){
+        free(product->name);
+        free(product);
+        return NULL;
+    }
+    product->product_data_copy_function = copy_function;
+    product->product_data_free_function = free_function;
+    product->product_data_get_price = price_function;
     product->amount_type = amount_type;
-    return product;
+    return (ASElement)product;
 }
 
-static Product copyProduct(Product product, MtmCopyData copy_function)
+static ASElement copyProduct(ASElement product)
 {
     Product product_copy = malloc(sizeof(*product_copy));
-    if(product_copy == NULL){
-        return NULL;
-    }
-    product_copy->name = malloc(sizeof(*(product->name)));
+    RETURN_IF_NULL(product_copy, NULL);
+    product_copy->name = malloc(sizeof(*(((Product)product)->name)));
     if(product_copy->name == NULL){
+        free(product);
         return NULL;
     }
-    strcpy(product_copy->name, product->name);
-    product_copy->id = product->id;
-    product_copy->product_data = copy_function(product->product_data);
+    strcpy(product_copy->name, ((Product)product)->name);
+    product_copy->id = ((Product)product)->id;
+    product_copy->product_data = ((Product)product)->
+                                product_data_copy_function(((Product)product)->product_data);
     if(product_copy->product_data == NULL){
+        free(product_copy->name);
+        free(product_copy);
         return NULL;
     }
-    product_copy->amount_type = product->amount_type;
+    product_copy->product_data_copy_function = ((Product)product)->product_data_copy_function;
+    product_copy->product_data_free_function = ((Product)product)->product_data_free_function;
+    product_copy->product_data_get_price = ((Product)product)->product_data_get_price;
+    product_copy->amount_type = ((Product)product)->amount_type;
+    return (ASElement)product_copy;
 }
 
-static void freeProduct(Product product, MtmFreeData free_function)
+static void freeProduct(ASElement product)
 {
-    free(product->name);
-    free_function(product->product_data);
+    free(((Product)product)->name);
+    ((Product)product)->product_data_free_function(((Product)product)->product_data);
     free(product);
 }
 
-static int compareProduct(Product product1, Product product2)
+static int compareProduct(ASElement product1, ASElement product2)
 {
-    return product1->id - product2->id;
+    return ((Product)product1)->id - ((Product)product2)->id;
 }
-
-typedef Product (*ProductCopyData)(Product, MtmCopyData);
-typedef void (*ProductFreeData)(Product, MtmFreeData);
-typedef int (*ProductCompareData)(Product, Product);
 
 typedef struct order_t
 {
@@ -68,41 +89,38 @@ typedef struct order_t
     AmountSet products;
 }*Order;
 
-static Order createOrder(const unsigned int id, ProductCopyData product_copy_function, 
-                        ProductFreeData product_free_function, ProductCompareData product_compare_function)
+static SetElement createOrder(const unsigned int id, CopyASElement product_copy_function, 
+                        FreeASElement product_free_function, CompareASElements product_compare_function)
 {
     Order order = malloc(sizeof(*order));
-    if(order == NULL){
-        return NULL;
-    }
+    RETURN_IF_NULL(order, NULL);
     order->id = id;
     order->products = asCreate(product_copy_function, product_free_function, product_compare_function);
-    return order;
+    return (SetElement)order;
 }
 
-static Order copyOrder(Order order)
+static SetElement copyOrder(SetElement order)
 {
     Order order_copy = malloc(sizeof(*order_copy));
-    if(order_copy == NULL){
-        return NULL;
-    } 
-    order_copy->id = order->id;
-    order_copy->products = asCopy(order->products);
-    if(order->products == NULL){
+    RETURN_IF_NULL(order_copy, NULL);
+    order_copy->id = ((Order)order)->id;
+    order_copy->products = asCopy(((Order)order)->products);
+    if(order_copy->products == NULL){
+        free(order_copy);
         return NULL;
     }
-    return order_copy;
+    return (SetElement)order_copy;
 }
 
-static void freeOrder(Order order)
+static void freeOrder(SetElement order)
 {
-    asDestroy(order->products);
+    asDestroy(((Order)order)->products);
     free(order);
 }
 
-static int compareOrder(Order order1, Order order2)
+static int compareOrder(SetElement order1, SetElement order2)
 {
-    return order1->id - order2->id;
+    return ((Order)order1)->id - ((Order)order2)->id;
 }
 
 struct Matamikya_t
@@ -116,9 +134,7 @@ struct Matamikya_t
 Matamikya matamikyaCreate()
 {
     Matamikya matamikya = malloc(sizeof(*matamikya));
-    if(matamikya == NULL){
-        return NULL;
-    }
+    RETURN_IF_NULL(matamikya, NULL);
     matamikya->storage = NULL;
     matamikya->orders = NULL;
     matamikya->profit = NULL;
@@ -128,9 +144,7 @@ Matamikya matamikyaCreate()
 
 void matamikyaDestroy(Matamikya matamikya)
 {
-    if(matamikya == NULL){
-        return;
-    }
+    RETURN_IF_NULL(matamikya,);
     asDestroy(matamikya->storage);
     setDestroy(matamikya->orders);
     asDestroy(matamikya->profit);
@@ -142,16 +156,21 @@ MatamikyaResult mtmNewProduct(Matamikya matamikya, const unsigned int id, const 
                               const MtmProductData customData, MtmCopyData copyData,
                               MtmFreeData freeData, MtmGetProductPrice prodPrice)
 {
-    if(matamikya == NULL){
-        return MATAMIKYA_NULL_ARGUMENT;
-    }
+    RETURN_IF_NULL(matamikya, MATAMIKYA_NULL_ARGUMENT);
+    Product product_to_add = createProduct(name, id, customData, amountType, copyData);
+    RETURN_IF_NULL(product_to_add, NULL);
+    ProductCopyData (product_copy_function)(product_to_add, copyData) = copyProduct;
     if(matamikya->storage == NULL){
-        matamikya->storage = asCreate()
+        matamikya->storage = asCreate();
     }
 }
 
-
 unsigned int mtmCreateNewOrder(Matamikya matamikya)
 {
-    
+    if(matamikya->orders == NULL){
+        matamikya->orders = setCreate(product_copy_function,
+                                product_free_function, product_compare_function);
+    }
+    Order new_order = createOrder(matamikya->next_order_id, );
+    setAdd(matamikya->orders, new_order);
 }
